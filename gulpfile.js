62fab98ac3f1c390015324f54d68bd074b2de757
cdq217 <nel@adm.ku.dk>
2019-03-19 11:30:58 +0100

    Gulpifying

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var paths = require('./paths.js'),
   browserSync = require('browser-sync').create(),
   reload = browserSync.reload,
   plumber = require('gulp-plumber'),
+  // shell = require('gulp-shell'),
   del = require('del'),
   less = require('gulp-less'),
   css = require('gulp-clean-css'),
@@ -11,7 +12,7 @@ var paths = require('./paths.js'),
   concat = require('gulp-concat'),
   jshint = require("gulp-jshint"),
   uglify = require('gulp-uglify'),
-  babel  = require('gulp-babel'),
+  babel = require('gulp-babel'),
   rename = require("gulp-rename"),
   notify = require("gulp-notify"),
   autoprefixer = require("gulp-autoprefixer"),
@@ -36,18 +37,24 @@ function serve(done) {
 
 // Clean build folder function:
 function cleanUp(cb) {
-    del.sync([paths.dist + '/**/*' , paths.docs + '/dist/**/*']);
-    cb();
+  del.sync([paths.dist + '/**/*', paths.docs + '/dist/**/*']);
+  cb();
 }
+
 function copyFiles() {
   return gulp
-    .src(paths.dist + '/**/*', {base: "."})
+    .src(paths.dist + '/**/*', {
+      base: "."
+    })
     .pipe(gulp.dest(paths.docs));
 }
 
 function copyFonts() {
   return gulp
-    .src(paths.fonts + '/**/*', {base: "."})
+    .src(paths.fonts + '/**/*', {
+      base: "."
+    })
+    .pipe(gulp.dest(paths.dist))
     .pipe(gulp.dest(paths.docs + '/dist'));
 }
 
@@ -55,11 +62,17 @@ function copyFonts() {
 function styles() {
   return gulp
     .src(paths.bsLess)
-    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
     .pipe(less())
+    .pipe(concat("bootstrap.css"))
+    .pipe(gulp.dest(paths.cssDest))
     .pipe(autoprefixer())
     .pipe(css())
-    .pipe(concat("bootstrap.min.css"))
+    .pipe(rename({
+      extname: '.min.css'
+    }))
     .pipe(gulp.dest(paths.cssDest))
     .pipe(browserSync.stream());
 }
@@ -67,19 +80,47 @@ function styles() {
 function fakStyles() {
   return gulp
     .src(paths.FAKstyles)
-    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
     .pipe(less())
     .pipe(autoprefixer())
     .pipe(css())
-    .pipe(gulp.dest(paths.dist + '/css/faculties'))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest(paths.cssDest + '/faculties'));
+}
+
+function docTypeStyles() {
+  return gulp
+    .src(paths.docTypesCss)
+    .pipe(autoprefixer())
+    .pipe(css({
+      inline: ['none']
+    }))
+    .pipe(gulp.dest(paths.cssDest + '/doctypes'));
+}
+
+function assetsCss() {
+  return gulp
+    .src(paths.assetsCss)
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
+    .pipe(autoprefixer())
+    .pipe(css())
+    .pipe(rename({
+      dirname: '',
+      extname: '.min.css'
+    }))
+    .pipe(gulp.dest(paths.cssDest));
 }
 
 // Lint scripts
 function scriptsLint() {
   return gulp
     .src(paths.jsSrc)
-    .pipe(plumber())
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
     .pipe(jshint())
     .pipe(jshint.reporter(jsStylish));
   //.pipe(jshint.reporter('fail'));
@@ -89,25 +130,49 @@ function scriptsLint() {
 function scripts() {
   return gulp
     .src(paths.jsSrc)
-    .pipe(plumber())
-    .pipe(concat("bootstrap.min.js"))
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
     .pipe(babel())
+    .pipe(concat("bootstrap.js"))
+    .pipe(gulp.dest(paths.jsDest))
     .pipe(uglify())
+    .pipe(rename({
+      extname: '.min.js'
+    }))
     .pipe(gulp.dest(paths.jsDest))
     .pipe(browserSync.stream());
 }
 
-function html() {
+function assetsScripts() {
   return gulp
-    .src(paths.gh_pages)
-    .pipe(htmlhint())
-    .pipe(htmlhint.reporter());
+    .src(paths.assetsJS)
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
+    .pipe(uglify())
+    .pipe(rename({
+      dirname: '',
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest(paths.jsDest));
+}
+
+function parseHtml() {
+  return gulp
+    .src(paths.docsParsedHtmml)
+    .pipe(htmlhint('.htmlhintrc'))
+    .pipe(htmlhint.failAfterError())
+    .pipe(htmlhint.reporter())
+    .pipe(browserSync.stream());
 }
 
 function docsCSS() {
   return gulp
     .src(paths.DocsCss)
-    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
     .pipe(autoprefixer())
     .pipe(css())
     .pipe(gulp.dest(paths.assets + '/src'))
@@ -115,27 +180,34 @@ function docsCSS() {
 }
 
 function watch() {
-  gulp
-    .watch(paths.DocsCss, docsCSS)
-    .on("change", reload);
-  gulp
-    .watch(paths.allLessFiles,  gulp.series(styles, fakStyles))
-    .on("change", reload);
-  gulp
-    .watch(paths.jsSrc, gulp.series(scriptsLint, scripts))
-    .on("change", reload);
-  // gulp
-  //   .watch(paths.html, renderHtml)
-  //   .on("change", reload);
+  gulp.watch(paths.allLessFiles, styles);
+  gulp.watch(paths.assetsCss, assetsCss);
+  gulp.watch(paths.FAKstyles, fakStyles);
+  gulp.watch(paths.docTypesCss, docTypeStyles);
+  gulp.watch(paths.jsSrc, gulp.series(scriptsLint, scripts));
+  gulp.watch(paths.assetsJS, assetsScripts);
+  gulp.watch(paths.DocsCss, docsCSS);
+  gulp.watch(paths.docsHtmml, parseHtml);
 }
 
+gulp.task("html", parseHtml);
+gulp.task("fonts", copyFonts);
 gulp.task("clean", cleanUp);
 gulp.task("docs", docsCSS);
+gulp.task("assets", gulp.series(assetsScripts, assetsCss));
 gulp.task("js", gulp.series(scriptsLint, scripts, copyFiles));
-gulp.task("styles", gulp.series(styles, fakStyles, copyFiles));
+gulp.task("styles", gulp.series(styles, fakStyles, docTypeStyles, copyFiles));
 gulp.task("copy", gulp.series(copyFiles, copyFonts));
 
-var build = gulp.parallel(cleanUp, "styles", "js");
-gulp.task('default', gulp.series(serve, watch, build, copyFiles, copyFonts));
+var build = gulp.parallel("styles", "js", "assets", "fonts");
+gulp.task('default', gulp.series(cleanUp, build, copyFiles, serve, watch));
 
+// export tasks
 exports.watch = watch;
+exports.scripts = scripts;
+exports.styles = styles;
+exports.assetsScripts = assetsScripts;
+exports.assetsCss = assetsCss;
+exports.cleanUp = cleanUp;
+exports.copyFiles = copyFiles;
+exports.copyFonts = copyFonts;
--- paths.js
+++ paths.js
@@ -2,15 +2,15 @@
 var paths = [];
 
 paths.root = "/";
-paths.gh_pages= '_gh_pages';
-paths.dist =  "dist";
+paths.gh_pages = '_gh_pages';
+paths.dist = "dist";
 paths.docs = "docs";
 paths.js = "js";
 paths.less = "less";
 paths.fonts = "fonts";
-paths.assets = "assets";
+paths.assets = "docs/assets";
 
-
+// Relevant js files
 paths.jsSrc = [
   'js/ku-global-footer.js',
   //'js/ku-mobile-menu.js',
@@ -28,22 +28,55 @@ paths.jsSrc = [
   //'js/affix.js',
   'js/ku-custom.js'
 ];
+
+// Output js path
 paths.jsDest = paths.dist + '/js';
 
+// Less files
 paths.allLessFiles = 'less/**/*.less';
 paths.bsLess = 'less/bootstrap.less';
 paths.cssDest = paths.dist + '/css';
 
+// FAK files
 paths.FAKstyles = 'less/faculties/*.less';
 
-paths.docsHtmml = [
-  "docs/_includes/**/*",
-  "docs/_layouts/**/*",
-  "docs/assets/**/*",
-  "docs/examples/**/*"
+// CMS Doctype files
+paths.docTypesCss = [
+  paths.assets + '/css/doctypes/**/*'
 ];
 
+// Docs input files
+paths.docsHtmml = [
+  paths.docs + '/_includes/**/*',
+  paths.docs + '/_layouts/**/*'
+];
+
+// Docs output files
+paths.docsParsedHtmml = '_gh_pages/*.html';
+
+// Docs css and js files
 paths.DocsCss = paths.assets + '/src/docs.css';
 paths.DocsJs = paths.assets + '/js/docs.min.js';
+
+// Assets scripts
+paths.assetsJS = [
+  paths.assets + '/js/datatables/datatables.js',
+  paths.assets + '/js/datetimepicker/bootstrap-datetimepicker.js',
+  paths.assets + '/js/multiple-select/multiple-select.js',
+  paths.assets + '/js/social-feeds/instagram/instagramByAccount.js',
+  paths.assets + '/js/social-feeds/instagram/instagramByHashtag.js',
+  paths.assets + '/js/videoControls/videoControls.js'
+];
+
+// Assets css
+paths.assetsCss = [
+  paths.assets + '/css/datatables/datatables.css',
+  paths.assets + '/css/multiple-select/multiple-select.css',
+  paths.assets + '/css/ku-dk-frontpage/ku-dk-frontpage.css',
+  paths.assets + '/css/social-feeds/instagram.css',
+  paths.assets + '/css/social-feeds/twitter.css',
+  paths.assets + '/css/print/print.css',
+  paths.assets + '/css/slider/slick-theme.css'
+];
 
 module.exports = paths;
